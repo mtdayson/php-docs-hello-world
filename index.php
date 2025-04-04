63e1d4e26a0b26e3f2087039994592034535df9f
mtdayson <atomic4fun@souunilavras.com>
2025-04-04 20:23:27 -0300

    Helo Azure

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo "
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Te Amo Minha Princesa</title>
+    <title>Hello Azure</title>
     <style>
         body {
             display: flex;
@@ -15,35 +15,17 @@ echo "
             background-color: #f4f4f4;
             margin: 0;
             font-family: Arial, sans-serif;
-            flex-direction: column;
         }
         h1 {
             font-size: 48px;
-            color: #ff4d6d;
-            display: flex;
-            align-items: center;
-        }
-        .heart {
-            font-size: 48px;
-            color: red;
-            margin-left: 10px;
-            animation: pulse 1.5s infinite alternate;
-        }
-        @keyframes pulse {
-            0% {
-                opacity: 0;
-                transform: scale(0.8);
-            }
-            100% {
-                opacity: 1;
-                transform: scale(1.2);
-            }
+            color: #333;
         }
     </style>
 </head>
 <body>
-    <h1>Te amo minha princesa <span class='heart'>❤️</span></h1>
+    <h1>Hello Azure!</h1>
 </body>
 </html>
 ";
 ?>
+
